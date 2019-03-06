064c2b1170fc4da9ec759b3920a1e7567b322f3c
szymonmenmen <szymonmenmen@gmail.com>
2019-03-06 21:29:38 +0100

    init maven project

---
+++ back-end/src/main/java/pl/polsl/geotag/GeotagApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.geotag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeotagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeotagApplication.class, args);
+    }
+
+}
