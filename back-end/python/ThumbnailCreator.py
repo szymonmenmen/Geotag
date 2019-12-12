5790808c05d885bfce6879dd0267f57411aa3c1c
daroface <dominik1050@gmail.com>
2019-12-12 22:57:00 +0100

    File refactor
    Delete redundant spaces and change variables names.

--- back-end/python/ThumbnailCreator.py
+++ back-end/python/ThumbnailCreator.py
@@ -2,21 +2,20 @@ import base64
 import io
 import json
 import sys
-
 import requests
 from PIL import Image
 
 
-def decode_img(msg):
+def decode_img(message):
     new_size = (300, 200)
-    msg = base64.b64decode(msg)
-    buf = io.BytesIO(msg)
-    img = Image.open(buf)
+    message = base64.b64decode(message)
+    buffor = io.BytesIO(message)
+    img = Image.open(buffor)
     img.thumbnail(new_size)
-    buf = io.BytesIO()
-    img.save(buf, 'PNG')
-    buf.seek(0)
-    img_bytes = buf.read()
+    buffor = io.BytesIO()
+    img.save(buffor, 'PNG')
+    buffor.seek(0)
+    img_bytes = buffor.read()
     base64_encoded_result_bytes = base64.b64encode(img_bytes)
     return base64_encoded_result_bytes.decode('utf-8')
 
