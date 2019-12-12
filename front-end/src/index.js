95bef480adb956f6c28fad0fe309c80ab08980e1
magdbry <madzik35#gmail.com>
2019-12-12 17:42:15 +0100

    removed comment and unused variable

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -6,8 +6,4 @@ import LocationService from './assets/js/service/location_service';
 
 require('html-loader!./templates/index.html');
 
-const app = new GeotagApp(new PhotoService(), new LocationService());
-
-// L.marker([51.5, -0.09]).addTo(map)
-//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-//     .openPopup();
+new GeotagApp(new PhotoService(), new LocationService());
